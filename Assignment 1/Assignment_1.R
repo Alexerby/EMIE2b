398202fa28ec85ef1863a826f5594f9dac15e9fc
Alerby <alexandereriksson740@outlook.com>
2023-05-08 15:49:10 +0200

    for loops for residuals

--- Assignment 1/Assignment_1.R
+++ Assignment 1/Assignment_1.R
@@ -1,6 +1,8 @@
 # Load required libraries
 library(Ecdat)
 library(dynlm)
+library(TS)
+
 
 # Load dataset IncomeUK (from package Ecdat)
 data(IncomeUK, package = "Ecdat")
@@ -9,34 +11,50 @@ data(IncomeUK, package = "Ecdat")
 #                                   QUESTION 1                                 #
 ################################################################################
 
-# Defining models
-model1 <- dynlm(income ~ L(income, 1), data = IncomeUK)
-model2 <- dynlm(income ~ L(income, 1:2), data = IncomeUK)
-model3 <- dynlm(income ~ L(income, 1:3), data = IncomeUK)
-model4 <- dynlm(income ~ L(income, 1:4), data = IncomeUK)
-model5 <- dynlm(income ~ L(income, 1:5), data = IncomeUK)
+model_list <- list()
 
+for (i in 1:5) {
+    # Fit the model with the current lag value
+    model <- dynlm(income ~ L(income, 1:i), data = IncomeUK)
 
-# Producing outputs by calling the summary() function
-summary(model1)
-summary(model2)
-summary(model3)
-summary(model4)
-summary(model5)
+    # Add the model object to the list
+    model_list[[i]] <- model
 
+    # Assign the model object to a variable
+    # with a name that follows the pattern "model_i"
+    assign(paste0("model_", i), model_list[[i]])
+}
 
 ################################################################################
 #                                   QUESTION 2                                 #
 ################################################################################
 
 # Calculating the AIC for the different models
-aic_values <- c(AIC(model1), AIC(model2), AIC(model3), AIC(model4), AIC(model5))
+aic_values <- c(
+    AIC(model_1),
+    AIC(model_2),
+    AIC(model_3),
+    AIC(model_4),
+    AIC(model_5)
+)
+
+aic_values
 
 # Determine the minimum value of the models
 best_model_aic <- which.min(aic_values)
 cat("Best model based on AIC: ", best_model_aic, "\n")
 
-
 ################################################################################
 #                                   QUESTION 3                                 #
 ################################################################################
+
+
+residuals_list <- list()
+
+for (i in 1:length(model_list)) {
+    residuals_list[[i]] <- residuals(model_list[[i]])
+    assign(paste0("residuals_model_", i), residuals_list[[i]])
+}
+
+
+TS::LjungBox(residuals_model_1)
