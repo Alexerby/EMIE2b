4e7621e8f8878b27cad2d68f3fd8b8fbd494d405
Alerby <alexandereriksson740@outlook.com>
2023-05-14 15:46:49 +0200

    order_of_integration defined and working properly

--- Assignments/Assignment 2/Assignment_2.R
+++ Assignments/Assignment 2/Assignment_2.R
@@ -10,7 +10,9 @@ data(ChickEgg, package = "lmtest")
 ################################################################################
 
 
-order_of_integration <- function(variable) {
+# Define function for checking the required order of integration using the augmented dickey fuller test
+
+order_of_integration <- function(variable, significance_level = 0.05, p_value = 1, difference = 0) {
     while (p_value >= significance_level) {
         difference <- difference + 1 # increment the difference by 1
 
@@ -19,11 +21,11 @@ order_of_integration <- function(variable) {
             break
         }
         chicken_diff <- diff(variable, differences = difference) # otherwise take difference of order d
-        adf_test <- adf.test(chicken_diff) # perform adf test
+        adf_test <- suppressWarnings(adf.test(chicken_diff)) # perform adf test
         p_value <- adf_test$p.value # assign the p_value to variable
 
         if (p_value < significance_level) {
-            cat("We need to difference the time series data of variable 'chicken'", difference, "time(s)")
+            cat("We need to difference the time series data of variable", substitute(variable), difference, "time(s) \n\n")
             break
         } else {
             print(paste("ADF test for", difference, "order difference: p-value =", p_value))
@@ -31,16 +33,9 @@ order_of_integration <- function(variable) {
     }
 }
 
-
-
-# Checking order of integration for chicken variable in dataset ChickEgg
-
-# Choose variable
 chicken <- ChickEgg[, "chicken"]
+egg <- ChickEgg[, "egg"]
 
-# Initialize necessary variables
-significance_level <- 0.05
-difference <- 0
-p_value <- 1
-
+# Checking order of integration for chicken and egg variable using our defined function
 order_of_integration(chicken)
+order_of_integration(egg)
