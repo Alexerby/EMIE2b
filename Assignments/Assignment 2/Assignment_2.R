91f5800c1ffc87a89a9ab13a02189a0880590285
Alerby <alexandereriksson740@outlook.com>
2023-05-14 17:19:06 +0200

    question 1 complete

--- Assignments/Assignment 2/Assignment_2.R
+++ Assignments/Assignment 2/Assignment_2.R
@@ -10,25 +10,40 @@ data(ChickEgg, package = "lmtest")
 ################################################################################
 
 
-# Define function for checking the required order of integration using the augmented dickey fuller test
-
-order_of_integration <- function(variable, significance_level = 0.05, p_value = 1, difference = 0) {
+# Define function for checking the required
+# order of integration using the augmented dickey fuller test
+order_of_integration <- function(variable,
+                                 significance_level = 0.05,
+                                 p_value = 1,
+                                 difference = 0) {
     while (p_value >= significance_level) {
         difference <- difference + 1 # increment the difference by 1
 
-        # Check difference against dataset to not take a difference greater than the length of the dataset
+        # Check difference against dataset to not
+        # take a difference greater than the length of the dataset
         if (difference > length(variable) - 1) {
             break
         }
-        chicken_diff <- diff(variable, differences = difference) # otherwise take difference of order d
+
+        # otherwise take difference of order d
+        chicken_diff <- diff(variable, differences = difference)
         adf_test <- suppressWarnings(adf.test(chicken_diff)) # perform adf test
         p_value <- adf_test$p.value # assign the p_value to variable
 
         if (p_value < significance_level) {
-            cat("We need to difference the time series data of variable", substitute(variable), difference, "time(s) \n\n")
+            cat(
+                "The order of integration for variable",
+                substitute(variable), "is", difference,
+                "\n\n"
+            )
             break
         } else {
-            print(paste("ADF test for", difference, "order difference: p-value =", p_value))
+            print(paste(
+                "ADF test for",
+                difference,
+                "order difference: p-value =",
+                p_value
+            ))
         }
     }
 }
@@ -36,6 +51,7 @@ order_of_integration <- function(variable, significance_level = 0.05, p_value = 
 chicken <- ChickEgg[, "chicken"]
 egg <- ChickEgg[, "egg"]
 
-# Checking order of integration for chicken and egg variable using our defined function
+# Checking order of integration for chicken and
+# egg variable using our defined function
 order_of_integration(chicken)
 order_of_integration(egg)
