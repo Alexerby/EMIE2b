2e96e71c91e597cf90cb29e87a760c6ffd3847b2
Alerby <alexandereriksson740@outlook.com>
2023-05-27 10:05:53 +0200

    plots fixed

--- Assignments/Assignment 3/Assignment 3 (using Rdata).R
+++ Assignments/Assignment 3/Assignment 3 (using Rdata).R
@@ -1,5 +1,4 @@
 
-
 # Load created dataset
 load("forecasts.Rdata")
 
@@ -8,14 +7,14 @@ origin_1 <- 2002 + 11 / 12
 par(mfrow = c(3, 2))
 
 # Plots for horizon = 1
-ts.plot(forecast_h1$re_arima_plot, main = "ARIMA forecasts (recursive)", col = "#008080")
+ts.plot(forecast_h1$re_arima_plot, main = "ARIMA forecast (recursive)", col = "#008080")
 lines(window(cpif, start = origin_1), col = "#FF7F50")
 legend("topleft", legend = c("Inflation rate", 
                             paste(12, "-step forecast")),
                             lty = 1,
                             col = c("#FF7F50", "#008080"))
 
-ts.plot(forecast_h1$re_ar1_plot, main = "ARIMA forecasts (recursive)", col = "#008080")
+ts.plot(forecast_h1$re_ar1_plot, main = "AR(1) forecast (recursive)", col = "#008080")
 lines(window(cpif, start = origin_1), col = "#FF7F50")
 legend("topleft", legend = c("Inflation rate", 
                             paste(12, "-step forecast")),
@@ -24,14 +23,14 @@ legend("topleft", legend = c("Inflation rate",
 
 
 # Plots for horizon = 12
-ts.plot(forecast_h12$re_arima_plot, main = "ARIMA forecasts (recursive)", col = "#008080")
+ts.plot(forecast_h12$re_arima_plot, main = "ARIMA forecast (recursive)", col = "#008080")
 lines(window(cpif, start = origin_1), col = "#FF7F50")
 legend("topleft", legend = c("Inflation rate", 
                             paste(12, "-step forecast")),
                             lty = 1,
                             col = c("#FF7F50", "#008080"))
 
-ts.plot(forecast_h12$re_ar1_plot, main = "ARIMA forecasts (recursive)", col = "#008080")
+ts.plot(forecast_h12$re_ar1_plot, main = "AR(1) forecast (recursive)", col = "#008080")
 lines(window(cpif, start = origin_1), col = "#FF7F50")
 legend("topleft", legend = c("Inflation rate", 
                             paste(12, "-step forecast")),
@@ -39,14 +38,14 @@ legend("topleft", legend = c("Inflation rate",
                             col = c("#FF7F50", "#008080"))
 
 # Plots for horizon = 24
-ts.plot(forecast_h24$re_arima_plot, main = "ARIMA forecasts (recursive)", col = "#008080")
+ts.plot(forecast_h24$re_arima_plot, main = "ARIMA forecast (recursive)", col = "#008080")
 lines(window(cpif, start = origin_1), col = "#FF7F50")
 legend("topleft", legend = c("Inflation rate", 
                             paste(12, "-step forecast")),
                             lty = 1,
                             col = c("#FF7F50", "#008080"))
 
-ts.plot(forecast_h24$re_ar1_plot, main = "ARIMA forecasts (recursive)", col = "#008080")
+ts.plot(forecast_h24$re_ar1_plot, main = "AR(1) forecast (recursive)", col = "#008080")
 lines(window(cpif, start = origin_1), col = "#FF7F50")
 legend("topleft", legend = c("Inflation rate", 
                             paste(12, "-step forecast")),
